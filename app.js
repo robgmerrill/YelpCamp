a500ec048222919239b3127bfb3ea4724b1c2a85
Rob Merrill <robgmerrill@gmail.com>
2016-04-20 14:28:24 -0700

    comments route built

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     Campground  = require("./models/campground"),
-    seedDB      = require("./seeds")
+    Comment     = require('./models/comment'),
+    seedDB      = require("./seeds") 
+
     
 mongoose.connect("mongodb://localhost/yelp_camp_v3");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -78,7 +80,28 @@ app.get('/campgrounds/:id/comments/new', function(req, res) {
   });
 });
 
-app.post
+app.post('/campgrounds/:id/comments', function(req, res) {
+  // lookup campground using ID
+  Campground.findById(req.params.id, function(err, campground) {
+    if(err) {
+      console.log(err);
+      res.redirect('/campgrounds');
+    } else {
+      Comment.create(req.body.comment, function(err, comment) {
+        if (err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect('/campgrounds/' + campground._id);
+        }
+      })
+    }
+  })
+  // created new comment
+  // connect new comment to campground
+  // redirect to show page of campground
+});
 
 
 app.listen(3000, function(){
