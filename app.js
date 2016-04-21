c5b051c0d939fc8098d25e8cae0ab2cd29a1b303
Rob Merrill <robgmerrill@gmail.com>
2016-04-21 04:51:38 -0700

    added logout logic

--- app.js
+++ app.js
@@ -29,6 +29,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.get("/", function(req, res){
     res.render("landing");
 });
@@ -40,7 +45,7 @@ app.get("/campgrounds", function(req, res){
        if(err){
            console.log(err);
        } else {
-          res.render("campgrounds/index",{campgrounds:allCampgrounds});
+          res.render("campgrounds/index",{campgrounds:allCampgrounds, currentUser: req.user});
        }
     });
 });
@@ -86,7 +91,7 @@ app.get("/campgrounds/:id", function(req, res){
 // Comments Routes
 // =========================
 
-app.get('/campgrounds/:id/comments/new', function(req, res) {
+app.get('/campgrounds/:id/comments/new', isLoggedIn, function(req, res) {
   // find campground by id
   Campground.findById(req.params.id, function(err, campground) {
     if(err) {
@@ -97,7 +102,7 @@ app.get('/campgrounds/:id/comments/new', function(req, res) {
   });
 });
 
-app.post('/campgrounds/:id/comments', function(req, res) {
+app.post('/campgrounds/:id/comments', isLoggedIn, function(req, res) {
   // lookup campground using ID
   Campground.findById(req.params.id, function(err, campground) {
     if(err) {
@@ -154,6 +159,19 @@ app.post("/login", passport.authenticate("local",
         failureRedirect: "/login"
     }), function(req, res){
 });
+
+// logout route
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/campgrounds");
+})
+
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
 
 app.listen(3000, function(){
    console.log("The YelpCamp Server Has Started!");
