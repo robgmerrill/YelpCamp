191c0e2d7dccfa6d4719fff7213a32ec081bd7ed
Rob Merrill <robgmerrill@gmail.com>
2016-04-22 08:37:41 -0700

    process.env.PORT for Heroku

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
    console.log("The YelpCamp Server Has Started!");
 });
